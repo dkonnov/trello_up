f15ffd7c62b5cad72b0e7c5739ad6a9c704f45af
Дмитрий Коннов <konnov@outlook.com>
2019-08-30 11:08:40 +0900

    small fixes

---
+++ public/trello-config.js
@@ -0,0 +1,3 @@
+var key = "2a754a93fa902b29d2694a2f71af3f83";
+var token = "b5123e80de5b5de7d21f46a754d8f97e6013facb5d0d6b5d2fcc2484b5530519";
+var board = "fsA5vKgk";
--- src/views/hollowCard.vue
+++ src/views/hollowCard.vue
@@ -84,11 +84,6 @@ import bootstrap from "bootstrap"
 import {http} from "./../http"
 import {eventEmitter} from "./../main"
 
-var key = "2a754a93fa902b29d2694a2f71af3f83";
-var token = "b5123e80de5b5de7d21f46a754d8f97e6013facb5d0d6b5d2fcc2484b5530519";
-var board = "fsA5vKgk";
-
-
 export default {
   name: 'hollowCard',
   data () {
