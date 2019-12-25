0ce3375514f9634420fa262a0a1b21de67b902db
Дмитрий Коннов <konnov@outlook.com>
2019-12-25 22:04:25 +0900

    доработка registration form

--- src/views/registration.vue
+++ src/views/registration.vue
@@ -7,81 +7,86 @@
         </div>
         <h4 class="info-title">Регистрация</h4>
       </div>
-
-      <div
-        class="input-group form-group label-floating"
-        :class="{ 'has-danger': $v.email.$error }"
-      >
-        <div class="input-group-prepend">
-          <span class="input-group-text">
-            <i class="material-icons">mail</i>
-          </span>
+      <div class="fields">
+        <div
+          class="input-group form-group label-floating"
+          :class="{ 'has-danger': $v.email.$error }"
+        >
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="material-icons">mail</i>
+            </span>
+          </div>
+          <input
+            v-model="email"
+            @input="$v.email.$touch"
+            type="email"
+            class="form-control"
+            placeholder="Электронная почта ..."
+          />
+          <button v-if="$v.email.$error" class="form-control-feedback">
+            <i class="material-icons">clear</i>
+          </button>
+          <small
+            v-if="$v.email.$error"
+            class="form-text text-muteds small-alert"
+            >Необходимо ввести адрес электронной почты, которого нет в
+            системе.</small
+          >
         </div>
-        <input
-          v-model="email"
-          @input="$v.email.$touch"
-          type="email"
-          class="form-control"
-          placeholder="Электронная почта ..."
-        />
-        <button v-if="$v.email.$error" class="form-control-feedback">
-          <i class="material-icons">clear</i>
-        </button>
-        <small v-if="$v.email.$error" class="form-text text-muteds small-alert"
-          >Необходимо ввести адрес электронной почты, которого нет в
-          системе.</small
+
+        <div
+          class="input-group form-group label-floating"
+          :class="{ 'has-danger': $v.password.$error }"
+        >
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="material-icons">lock_outline</i>
+            </span>
+          </div>
+          <input
+            v-model="password"
+            type="password"
+            @input="$v.password.$touch"
+            class="form-control"
+            placeholder="Пароль ..."
+          />
+          <button v-if="$v.password.$error" class="form-control-feedback">
+            <i class="material-icons">clear</i>
+          </button>
+        </div>
+        <small
+          v-if="$v.password.$error"
+          class="form-text text-muteds small-alert"
+          >Минимум 6 символов.</small
+        >
+
+        <div
+          class="input-group form-group label-floating"
+          :class="{ 'has-danger': $v.password2.$error }"
+        >
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="material-icons">lock_outline</i>
+            </span>
+          </div>
+          <input
+            v-model="password2"
+            type="password"
+            @input="$v.password2.$touch"
+            class="form-control"
+            placeholder="Пароль еще раз ..."
+          />
+          <button v-if="$v.password2.$error" class="form-control-feedback">
+            <i class="material-icons">clear</i>
+          </button>
+        </div>
+        <small
+          v-if="$v.password2.$error"
+          class="form-text text-muteds small-alert"
+          >Пароли не совпадают.</small
         >
       </div>
-
-      <div
-        class="input-group form-group label-floating"
-        :class="{ 'has-danger': $v.password.$error }"
-      >
-        <div class="input-group-prepend">
-          <span class="input-group-text">
-            <i class="material-icons">lock_outline</i>
-          </span>
-        </div>
-        <input
-          v-model="password"
-          type="password"
-          @input="$v.password.$touch"
-          class="form-control"
-          placeholder="Пароль ..."
-        />
-        <button v-if="$v.password.$error" class="form-control-feedback">
-          <i class="material-icons">clear</i>
-        </button>
-      </div>
-      <small v-if="$v.password.$error" class="form-text text-muteds small-alert"
-        >Минимум 6 символов.</small
-      >
-
-      <div
-        class="input-group form-group label-floating"
-        :class="{ 'has-danger': $v.password2.$error }"
-      >
-        <div class="input-group-prepend">
-          <span class="input-group-text">
-            <i class="material-icons">lock_outline</i>
-          </span>
-        </div>
-        <input
-          v-model="password2"
-          type="password"
-          @input="$v.password2.$touch"
-          class="form-control"
-          placeholder="Пароль еще раз ..."
-        />
-        <button v-if="$v.password2.$error" class="form-control-feedback">
-          <i class="material-icons">clear</i>
-        </button>
-      </div>
-      <small
-        v-if="$v.password2.$error"
-        class="form-text text-muteds small-alert"
-        >Пароли не совпадают.</small
-      >
       <br />
 
       <button
@@ -146,6 +151,10 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.input-group-text
+  color: #999
+.fields
+  margin-left: -50px
 .registration
   padding-left: 100px
   padding-right: 100px
