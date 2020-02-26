6a8bc25c26149dd4901b6d7f40471f8f9519ccd1
Дмитрий Коннов <konnov@outlook.com>
2020-02-26 14:27:09 +0900

    backgrounds window fixed

--- src/views/hollow.vue
+++ src/views/hollow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row">
+  <div class="row hollow">
     <div class="col-md-6">
       <div class="info info-horizontal" stype="padding: 0px 0 30px;">
         <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="1s">
@@ -143,6 +143,6 @@
 </template>
 
 <style lang="sass">
-.row
+.hollow
   min-width: 1024px
 </style>
